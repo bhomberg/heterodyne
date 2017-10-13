43a82c29bede0a8888216e82421baec71a5ba9da
Bianca Homberg <bhomberg@mit.edu>
2017-10-12 22:19:18 -0700

    test use case for pololu leds

--- engine-room/engine_arduino/engine_arduino.ino
+++ engine-room/engine_arduino/engine_arduino.ino
@@ -1,6 +1,7 @@
 #include <PololuLedStrip.h>
 
-
+PololuLedStrip<2> leds;
+rgb_color colors[4];
 
 void setup() 
 {
@@ -16,14 +17,49 @@ void loop()
   digitalWrite(LED_BUILTIN, LOW);
   delay(2000);
   
+  colors[0] = getRed();
+  colors[1] = getBlue();
+  colors[2] = getGreen();
+  colors[3] = getYellow();
   
+  leds.write(colors, 4);
 
   Serial.println('red'); // m is char*
 }
 
-void red()
+rgb_color getRed() 
 {
-  
+  rgb_color color;
+  color.red = 255;
+  color.blue = 0;
+  color.green = 0;
+  return color;
 }
 
+rgb_color getBlue() 
+{
+  rgb_color color;
+  color.red = 0;
+  color.blue = 255;
+  color.green = 0;
+  return color;
+}
+
+rgb_color getGreen() 
+{
+  rgb_color color;
+  color.red = 0;
+  color.blue = 0;
+  color.green = 255;
+  return color;
+}
+
+rgb_color getYellow() 
+{
+  rgb_color color;
+  color.red = 255;
+  color.blue = 0;
+  color.green = 255;
+  return color;
+}
 
