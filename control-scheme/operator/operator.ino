c8f232d2095060ee278fc9a6a03a5ebd909cefae
Bianca Homberg <bhomberg@mit.edu>
2017-10-24 23:35:13 -0700

    make operator arduino code compile

--- control-scheme/operator/operator.ino
+++ control-scheme/operator/operator.ino
@@ -1,4 +1,4 @@
-const int buttons[] = {2, 3, 4, 5, 6, 7}
+const int buttons[] = {2, 3, 4, 5, 6, 7};
 
 void setup() {
   for(int i=0; i < 6; i++)
@@ -10,7 +10,7 @@ void setup() {
 void loop() {
   for(int i=0; i<6; i++)
   {
-    s = digitalRead(buttons[i]);
+    int s = digitalRead(buttons[i]);
 
     // button is on
     if(s == HIGH)
